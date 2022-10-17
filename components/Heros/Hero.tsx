9873a92b7fa5259dabff7ae187fb4fba62eed786
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-10-17 23:58:54 +0700

    readjust with motion div to avoid duplicate motion

--- components/Heros/Hero.tsx
+++ components/Heros/Hero.tsx
@@ -8,18 +8,19 @@ const Hero = () => {
     return (
         <div className="flex h-screen flex-col items-center justify-center gap-1 overflow-hidden text-center">
             <Avatar />
-            <motion.h1
+            <motion.div
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 1 }}
-                className="bg-gradient-to-l from-gradient-blue-apple-from via-gradient-blue-apple-via to-gradient-blue-apple-to bg-clip-text text-xl font-extrabold uppercase tracking-widest text-transparent md:text-3xl md:tracking-[13px]"
             >
-                Hao Nguyen
-                <br />
-                <span className="bg-gradient-to-r from-gradient-blue-apple-from via-gradient-blue-apple-via to-gradient-blue-apple-to bg-clip-text tracking-wide">
-                    Front-end Developer
-                </span>
-            </motion.h1>
+                <h1 className="bg-gradient-to-l from-gradient-blue-apple-from via-gradient-blue-apple-via to-gradient-blue-apple-to bg-clip-text text-xl font-extrabold uppercase tracking-widest text-transparent md:text-3xl md:tracking-[13px]">
+                    Hao Nguyen
+                    <br />
+                    <span className="bg-gradient-to-r from-gradient-blue-apple-from via-gradient-blue-apple-via to-gradient-blue-apple-to bg-clip-text tracking-wide">
+                        Front-end Developer
+                    </span>
+                </h1>
+            </motion.div>
             <BackgroundCircles />
             <TypeWriter />
         </div>
