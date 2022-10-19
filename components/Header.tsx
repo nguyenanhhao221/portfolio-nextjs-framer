6784fa13d5f5ea34d191e345dfe3386fbe3b3d4f
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-10-19 19:26:13 +0700

    refactor use react-icons pkg instead of react-social-icons due to it easier to change fill color -Readjust header to use react-icons -Add link for each social icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,44 +1,83 @@
 import React from 'react';
-import { SocialIcon } from 'react-social-icons';
 import { motion } from 'framer-motion';
 import Toggle from './utils/Toggle';
-
+import {
+    AiFillFacebook,
+    AiFillGithub,
+    AiFillLinkedin,
+    AiFillTwitterSquare,
+} from 'react-icons/ai';
+import Link from 'next/link';
 export const Header = () => {
     return (
-        <header className="sticky top-0  px-4">
+        <header className="sticky top-0 w-full p-4">
             <div className="mx-auto flex w-full items-center justify-between gap-2 xl:max-w-[80%]">
-                <motion.div
+                <motion.ul
                     initial={{ x: -500, opacity: 0, scale: 0.5 }}
                     animate={{ x: 0, opacity: 1, scale: 1 }}
                     transition={{ duration: 1, delay: 0.5 }}
                     className="social-medias flex gap-1"
                 >
-                    <SocialIcon
-                        fgColor="gray"
-                        bgColor="transparent"
-                        className="motion-safe:hover:animate-bounce motion-safe:focus:animate-bounce"
-                        url="https://www.facebook.com/haonguyen11295"
-                    />
-                    <SocialIcon
-                        fgColor="gray"
-                        bgColor="transparent"
-                        className="motion-safe:hover:animate-bounce motion-safe:focus:animate-bounce"
-                        url="https://www.linkedin.com/in/haonguyen-5a08321a9/"
-                    />
-
-                    <SocialIcon
-                        className="motion-safe:hover:animate-bounce motion-safe:focus:animate-bounce"
-                        fgColor="gray"
-                        bgColor="transparent"
-                        url="https://www.github.com/nguyenanhhao221"
-                    />
-                    <SocialIcon
-                        className="motion-safe:hover:animate-bounce motion-safe:focus:animate-bounce"
-                        fgColor="gray"
-                        bgColor="transparent"
-                        url="https://www.twitter.com/nguyenanhhao221"
-                    />
-                </motion.div>
+                    <motion.li className="cursor-pointer">
+                        <Link
+                            href="https://facebook.com/haonguyen11295"
+                            passHref
+                            prefetch={false}
+                        >
+                            <a
+                                target="_blank"
+                                title="Go to Facebook"
+                                rel="noopener noreferrer"
+                            >
+                                <AiFillFacebook className="h-8 w-8  fill-neutral-700 hover:fill-[#1877f2]" />
+                            </a>
+                        </Link>
+                    </motion.li>
+                    <motion.li className="cursor-pointer">
+                        <Link
+                            href={'https://twitter.com/nguyenanhhao112'}
+                            passHref
+                        >
+                            <a
+                                target="_blank"
+                                title="Go to Twitter"
+                                rel="noopener noreferrer"
+                            >
+                                <AiFillTwitterSquare className="h-8 w-8  fill-neutral-700 hover:fill-[#1da1f2]" />
+                            </a>
+                        </Link>
+                    </motion.li>
+                    <motion.li className="cursor-pointer">
+                        <Link
+                            href={
+                                'https://www.linkedin.com/in/haonguyen-5a08321a9/'
+                            }
+                            passHref
+                        >
+                            <a
+                                target="_blank"
+                                title="Go to LinkedIn"
+                                rel="noopener noreferrer"
+                            >
+                                <AiFillLinkedin className="h-8 w-8  fill-neutral-700 hover:fill-[#0077b5]" />
+                            </a>
+                        </Link>
+                    </motion.li>
+                    <motion.li className="cursor-pointer">
+                        <Link
+                            href={'https://github.com/nguyenanhhao221'}
+                            passHref
+                        >
+                            <a
+                                title="Go to Github"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <AiFillGithub className="h-8 w-8  fill-neutral-700 hover:fill-[#333] dark:hover:fill-[#f5f5f5]" />
+                            </a>
+                        </Link>
+                    </motion.li>
+                </motion.ul>
                 <motion.div
                     initial={{
                         x: 500,
