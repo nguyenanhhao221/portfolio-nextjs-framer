c5ae3f96daac2dc5c307b9fd280fd2b76b63551d
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-10-30 23:07:08 +0700

    add header and icons, modify project abit

--- components/Projects/ProjectCard.tsx
+++ components/Projects/ProjectCard.tsx
@@ -2,33 +2,58 @@ import React from 'react';
 import Image from 'next/future/image';
 import { z } from 'zod';
 import { TProjectType } from './Projects';
-import { CTAButtons } from '../CTAButtons/CTAButtons';
-import { CodeBracketIcon } from '@heroicons/react/24/outline';
+import { motion } from 'framer-motion';
+import { CodeBracketIcon, TvIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 type Props = {
   project: z.infer<typeof TProjectType>;
 };
 
 export const ProjectCard = ({ project }: Props) => {
-  const { description, id, links, name, image } = project;
+  const { description, links, name, image } = project;
   return (
-    <div className="flex flex-col gap-4 py-4">
-      <Image alt={name} src={image}></Image>
-      <div className="">
-        <h2 className="text-center text-xl font-bold tracking-widest">
+    <div className="flex flex-col items-center gap-4">
+      <div className="h-72 overflow-hidden">
+        <Image
+          className="max-h-72 rounded-lg object-cover "
+          alt={name}
+          src={image}
+        ></Image>
+      </div>
+      <div>
+        <h2 className="text-center text-xl font-bold tracking-widest lg:text-3xl">
           {name}
         </h2>
-        <p className="text-center text-slate-400">{description}</p>
+        <p className="text-center text-slate-400 lg:px-4 lg:py-4 lg:text-lg">
+          {description}
+        </p>
       </div>
-      <div className="flex">
-        <CTAButtons
-          backHome={false}
-          CTAProps={{ btnTitle: 'Source Code', Icon: CodeBracketIcon }}
-        />
-        <CTAButtons
-          backHome={false}
-          CTAProps={{ btnTitle: 'Source Code', Icon: CodeBracketIcon }}
-        />
+      <div className="flex gap-2 py-8">
+        <div>
+          <Link href={links.github} passHref title="Source code">
+            <a
+              className="flex items-center gap-2 rounded-md p-2 underline decoration-gradient-purple-apple-from underline-offset-4  motion-safe:hover:scale-110"
+              target={'_blank'}
+              rel="noopener noreferrer"
+            >
+              <CodeBracketIcon className="hidden h-4 w-4 md:block" />
+              <p className="capitalize">source code</p>
+            </a>
+          </Link>
+        </div>
+        <div>
+          <Link href={links.deployment} passHref title="Live Demo">
+            <motion.a
+              className="flex items-center gap-2 rounded-md p-2 underline decoration-gradient-purple-apple-from underline-offset-4  motion-safe:hover:scale-110"
+              target={'_blank'}
+              rel="noopener noreferrer"
+            >
+              <TvIcon className="hidden h-4 w-4 md:block" />
+              <p className="capitalize">live demo</p>
+            </motion.a>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { SectionTitle } from '../SectionTitle/SectionTitle';
 import HuluDisplayImg from '../../public/HuluDisplayImg.png';
+import CryptoverseImg from '../../public/cryptoverse.png';
 import { z } from 'zod';
 import { ProjectCard } from './ProjectCard';
-import { StaticImageData } from 'next/future/image';
+import { motion } from 'framer-motion';
 
 export const TProjectType = z.object({
   id: z.string(),
@@ -30,30 +31,73 @@ const allProjects = z.array(TProjectType).parse([
       deployment: 'https://projects.haonguyen.tech',
     },
   },
-  //   {
-  //     id: 'cryptoverse',
-  //     name: 'Cryptoverse',
-  //     image: HuluDisplayImg,
-  //     description:
-  //       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cupiditate dicta explicabo optio alias a ratione, provident doloribus voluptatum neque non ab quidem animi, eveniet accusantium facere aliquid accusamus sit?',
-  //     links: {
-  //       github: 'https://github.com/nguyenanhhao221/hulu-clone',
-  //       deployment: 'https://projects.haonguyen.tech',
-  //     },
-  //   },
+  {
+    id: 'cryptoverse',
+    name: 'Cryptoverse',
+    image: CryptoverseImg,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cupiditate dicta explicabo optio alias a ratione, provident doloribus voluptatum neque non ab quidem animi, eveniet accusantium facere aliquid accusamus sit?',
+    links: {
+      github: 'https://github.com/nguyenanhhao221/hulu-clone',
+      deployment: 'https://projects.haonguyen.tech',
+    },
+  },
+  {
+    id: 'Portfolio',
+    name: 'Hao Portfolio',
+    image: HuluDisplayImg,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cupiditate dicta explicabo optio alias a ratione, provident doloribus voluptatum neque non ab quidem animi, eveniet accusantium facere aliquid accusamus sit?',
+    links: {
+      github: 'https://github.com/nguyenanhhao221/hulu-clone',
+      deployment: 'https://projects.haonguyen.tech',
+    },
+  },
 ]);
 
 export const Projects = () => {
+  const listRef = useRef<HTMLUListElement>(null);
+  // use to scroll to item in mobile view
+  function scrollToIndex(index: number) {
+    const listNode = listRef.current;
+    const itemList = listNode?.querySelectorAll('li')[index];
+    return itemList?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center',
+    });
+  }
   return (
-    <div className="flex h-screen flex-col items-center justify-center px-4">
+    <div className="flex flex-col items-center justify-center px-4">
       <SectionTitle title="Projects" />
-      <ul>
-        {allProjects.map((project) => (
-          <li key={project.id}>
-            <ProjectCard project={project} />
-          </li>
-        ))}
-      </ul>
+      <div className="carousel w-full cursor-grab lg:flex lg:cursor-default lg:place-content-center">
+        {/* <div className="flex w-full items-center justify-center gap-4 py-2 lg:hidden">
+          {allProjects.map((project, index) => (
+            <button
+              type="button"
+              key={project.id}
+              onClick={() => {
+                scrollToIndex(index);
+              }}
+              title="move to item"
+              className="h-4 w-4 rounded-full bg-red-400"
+            ></button>
+          ))}
+        </div> */}
+        <motion.ul
+          ref={listRef}
+          className="inner-carousel flex w-full snap-x snap-mandatory gap-4 overflow-y-hidden overflow-x-scroll py-2 scrollbar-hide lg:grid lg:w-[80%] lg:grid-cols-2 lg:place-content-center lg:gap-16 lg:overflow-y-visible"
+        >
+          {allProjects.map((project) => (
+            <motion.li
+              key={project.id}
+              className="w-full flex-shrink-0 cursor-pointer snap-center snap-always rounded-lg shadow-2xl transition-transform lg:border lg:border-slate-400 lg:border-opacity-30 lg:motion-safe:hover:border-opacity-100"
+            >
+              <ProjectCard project={project} />
+            </motion.li>
+          ))}
+        </motion.ul>
+      </div>
     </div>
   );
 };
