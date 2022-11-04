dde3fbb51078e61a3fff7e7de880a2354a3e660d
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-11-05 04:07:50 +0700

    styles: add blur effect for About secton -Modify CTA button

--- components/About.tsx
+++ components/About.tsx
@@ -2,7 +2,7 @@ import Image from 'next/future/image';
 import React from 'react';
 import HaoAva from '../public/Hao-Ava.jpg';
 import { CTAButtons } from './CTAButtons';
-import { DocumentArrowDownIcon } from '@heroicons/react/24/outline';
+import { ArrowDownCircleIcon } from '@heroicons/react/24/outline';
 import { motion } from 'framer-motion';
 import { SectionTitle } from './SectionTitle';
 import { Quotes } from './Quotes';
@@ -10,19 +10,24 @@ import { Quotes } from './Quotes';
 export const About = () => {
   return (
     <div className="mt-2 flex h-screen flex-col items-center justify-center">
-      <SectionTitle title="about" />
-      <div className="py-2 text-center font-mono text-sm leading-7 tracking-wider text-slate-400 md:text-base">
+      <SectionTitle title="about me" />
+      <div className="py-2 text-center font-mono text-sm leading-7 tracking-wider text-slate-400 md:py-6 md:text-base">
         <Quotes />
       </div>
-      <div className="mx-auto flex flex-col items-center gap-6 md:max-w-[80%] md:flex-row md:shadow-2xl">
+      <div className="mx-auto flex flex-col items-center gap-6 md:relative md:z-0 md:flex-row md:items-stretch md:rounded-lg md:border md:border-stone-900 md:shadow-2xl lg:max-w-[70%]">
+        <div className="img-bg-blur absolute -z-10 h-full w-full animate-pulse rounded-md bg-gradient-to-tl from-gradient-blue-apple-via to-gradient-purple-apple-to opacity-50 blur-3xl md:flex md:items-center">
+          <div className="h-1/2 w-full bg-black bg-opacity-70"></div>
+        </div>
         <motion.div
           initial={{ opacity: 0, x: -200 }}
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 1.2 }}
+          className="relative flex h-full flex-col items-center justify-center md:static md:flex-none"
         >
+          <div className="img-bg-blur -z-1 absolute h-52 w-52 animate-pulse rounded-full bg-gradient-to-tr from-gradient-blue-apple-via to-gradient-purple-apple-to opacity-50 blur-3xl md:hidden md:h-0 md:w-0"></div>
           <Image
-            className="h-48 w-48 rounded-full object-cover object-center md:h-auto md:max-w-sm md:basis-1/3 md:rounded-md"
+            className="h-48 w-48 rounded-full object-cover object-center backdrop-blur-md md:h-auto md:max-w-sm md:basis-1/3 md:rounded-md"
             alt="Hao"
             src={HaoAva}
             placeholder="blur"
@@ -33,17 +38,34 @@ export const About = () => {
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 1.2 }}
-          className="flex h-full flex-col items-center gap-6 px-2 md:items-start md:justify-center md:gap-4 xl:text-lg"
+          className="flex h-full flex-col items-center gap-6 px-2 md:items-start md:justify-center md:gap-4 md:py-4 lg:w-[70%] xl:text-lg"
         >
-          <p className="text-center font-serif text-sm font-light leading-relaxed tracking-wide md:text-left md:text-base">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-            repudiandae molestias necessitatibus fugit. Aut quam quis alias
-            libero sequi qui, fugiat distinctio veritatis.
+          <p className="text-center font-serif text-sm font-light leading-relaxed md:text-left md:text-base">
+            I&apos;m a Front-End Developer based in Vietnam. 🇻🇳
+            <br />I have been falling in love with coding ever since I found out
+            the amazing things you can do with just a few line of code.
+            <br />
+            That feeling of seeing everything coming to life in the browser from
+            your idea after hours of working and debugging is something that is
+            so addicted.
+            <br />
+            Anyways, thank you so much for visiting my site, you can find some
+            of my works below. And if you ever fancy a quick chat, I&apos;m
+            always just one
+            <a
+              className="text-sky-500 hover:underline hover:underline-offset-4 "
+              href={'#contact'}
+            >
+              {' '}
+              click{' '}
+            </a>
+            away
           </p>
           <CTAButtons
-            btnTitle="Download CV"
-            Icon={DocumentArrowDownIcon}
-            className="group rounded-md bg-gradient-purple-apple-via ring-1 ring-gradient-purple-apple-from transition-colors hover:bg-gradient-purple-apple-from hover:text-white"
+            btnTitle="My Projects"
+            href="#projects"
+            Icon={ArrowDownCircleIcon}
+            className="group rounded-md bg-gradient-purple-apple-via text-center ring-1 ring-gradient-purple-apple-from transition-colors hover:bg-gradient-purple-apple-from hover:text-white"
           />
         </motion.div>
       </div>
--- components/CTAButtons.tsx
+++ components/CTAButtons.tsx
@@ -1,21 +1,20 @@
 import Link from 'next/link';
 import React from 'react';
+
 type Props = {
   btnTitle: string;
+  href: string;
   Icon: (props: React.ComponentProps<'svg'>) => JSX.Element;
   className: string;
 };
+
 //TODO update correct link for button
-export const CTAButtons = ({ btnTitle, Icon, className }: Props) => {
+export const CTAButtons = ({ href, btnTitle, Icon, className }: Props) => {
   return (
     <button type="button" className={className}>
-      <Link href={'/'} passHref>
+      <Link href={href} passHref>
         <div className="flex flex-row items-center gap-2 py-2 px-4">
-          {Icon && (
-            <Icon
-              className={`h-4 w-4 motion-safe:group-hover:animate-bounce`}
-            />
-          )}
+          {Icon && <Icon className={`h-6 w-6 animate-bounce`} />}
           <span className="text-sm md:text-base">{btnTitle}</span>
         </div>
       </Link>
