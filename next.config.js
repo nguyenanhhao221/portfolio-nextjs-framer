3a565bf5217deb47e9b4907d385488788e07d18f
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-11-04 15:59:49 +0700

    feat: add webpack bundle anaylizer and script to check JS bundle size and remove unused Javascript

--- next.config.js
+++ next.config.js
@@ -11,5 +11,8 @@ const nextConfig = {
     ],
   },
 };
-
-module.exports = nextConfig;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer(nextConfig);
