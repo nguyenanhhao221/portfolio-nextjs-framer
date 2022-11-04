a049dcf5d729240d6a6a23e0edf59a4b9c828715
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-11-04 15:57:53 +0700

    refactor: move next/link away from Header and use a tag only -Next/link with nested a tag will cause breaking error in next major release of NextJS -Just use <a> tag for external link because Next/link not provide any benefit

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,66 +8,58 @@ import {
   AiFillLinkedin,
   AiFillTwitterSquare,
 } from 'react-icons/ai';
-import Link from 'next/link';
 
 export const Header = () => {
   return (
     <header className="fixed left-0 right-0 top-0 w-full p-4 lg:backdrop-blur-sm">
-      <div className="mx-auto flex w-full items-center justify-between gap-2 xl:max-w-[80%]">
-        <motion.ul
-          initial={{ x: -500, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ duration: 1, delay: 0.5 }}
-          className="social-medias flex gap-1"
-        >
-          <motion.li className="cursor-pointer">
-            <Link
+      <motion.div
+        initial={{ x: -500, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1, delay: 0.5 }}
+        className="mx-auto flex w-full items-center justify-between gap-2 xl:max-w-[80%]"
+      >
+        <ul className="social-medias flex gap-1">
+          <li className="cursor-pointer">
+            <a
+              target="_blank"
+              title="Go to Facebook"
               href="https://facebook.com/haonguyen11295"
-              passHref
-              prefetch={false}
+              rel="noopener noreferrer"
             >
-              <a
-                target="_blank"
-                title="Go to Facebook"
-                rel="noopener noreferrer"
-              >
-                <AiFillFacebook className="h-8 w-8  fill-neutral-700 hover:fill-[#1877f2]" />
-              </a>
-            </Link>
-          </motion.li>
-          <motion.li className="cursor-pointer">
-            <Link href={'https://twitter.com/nguyenanhhao112'} passHref>
-              <a
-                target="_blank"
-                title="Go to Twitter"
-                rel="noopener noreferrer"
-              >
-                <AiFillTwitterSquare className="h-8 w-8  fill-neutral-700 hover:fill-[#1da1f2]" />
-              </a>
-            </Link>
-          </motion.li>
-          <motion.li className="cursor-pointer">
-            <Link
+              <AiFillFacebook className="h-8 w-8  fill-neutral-700 hover:fill-[#1877f2]" />
+            </a>
+          </li>
+          <li className="cursor-pointer">
+            <a
+              target="_blank"
+              title="Go to Twitter"
+              href={'https://twitter.com/nguyenanhhao112'}
+              rel="noopener noreferrer"
+            >
+              <AiFillTwitterSquare className="h-8 w-8  fill-neutral-700 hover:fill-[#1da1f2]" />
+            </a>
+          </li>
+          <li className="cursor-pointer">
+            <a
               href={'https://www.linkedin.com/in/haonguyen-5a08321a9/'}
-              passHref
+              target="_blank"
+              title="Go to LinkedIn"
+              rel="noopener noreferrer"
             >
-              <a
-                target="_blank"
-                title="Go to LinkedIn"
-                rel="noopener noreferrer"
-              >
-                <AiFillLinkedin className="h-8 w-8  fill-neutral-700 hover:fill-[#0077b5]" />
-              </a>
-            </Link>
-          </motion.li>
-          <motion.li className="cursor-pointer">
-            <Link href={'https://github.com/nguyenanhhao221'} passHref>
-              <a title="Go to Github" target="_blank" rel="noopener noreferrer">
-                <AiFillGithub className="h-8 w-8  fill-neutral-700 hover:fill-[#333] dark:hover:fill-[#f5f5f5]" />
-              </a>
-            </Link>
-          </motion.li>
-        </motion.ul>
+              <AiFillLinkedin className="h-8 w-8  fill-neutral-700 hover:fill-[#0077b5]" />
+            </a>
+          </li>
+          <li className="cursor-pointer">
+            <a
+              href={'https://github.com/nguyenanhhao221'}
+              title="Go to Github"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AiFillGithub className="h-8 w-8  fill-neutral-700 hover:fill-[#333] dark:hover:fill-[#f5f5f5]" />
+            </a>
+          </li>
+        </ul>
         <motion.div
           initial={{
             x: 500,
@@ -82,7 +74,7 @@ export const Header = () => {
         >
           <Toggle />
         </motion.div>
-      </div>
+      </motion.div>
     </header>
   );
 };
