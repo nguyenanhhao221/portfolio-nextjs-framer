aa3cd68fa64d132b9294e754e6ff92ea52a062a3
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-11-05 04:40:36 +0700

    refactor: change Navbar link to Contact me instead of Experience for now

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,27 +2,27 @@ import React from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 export const Navbar = () => {
-    return (
-        <motion.nav
-            className="mt-4"
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 1 }}
-        >
-            <ul className="flex items-center justify-evenly gap-3  text-center text-sm capitalize">
-                <li className="p-1 text-center hover:border-b focus:border-b dark:text-gray-400">
-                    <Link href={'#about'}>about </Link>
-                </li>
-                <li className="p-1 text-center hover:border-b focus:border-b dark:text-gray-400">
-                    <Link href={'#projects'}>projects</Link>
-                </li>
-                <li className="p-1 text-center hover:border-b focus:border-b dark:text-gray-400">
-                    <Link href={'#skills'}>skills</Link>
-                </li>
-                <li className="p-1 text-center hover:border-b focus:border-b dark:text-gray-400">
-                    <Link href={'#experience'}>experience</Link>
-                </li>
-            </ul>
-        </motion.nav>
-    );
+  return (
+    <motion.nav
+      className="mt-4"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <ul className="flex items-center justify-evenly gap-3  text-center text-sm capitalize">
+        <li className="p-1 text-center focus:border-b hover:border-b dark:text-gray-400">
+          <Link href={'#about'}>about </Link>
+        </li>
+        <li className="p-1 text-center focus:border-b hover:border-b dark:text-gray-400">
+          <Link href={'#projects'}>projects</Link>
+        </li>
+        <li className="p-1 text-center focus:border-b hover:border-b dark:text-gray-400">
+          <Link href={'#skills'}>skills</Link>
+        </li>
+        <li className="p-1 text-center focus:border-b hover:border-b dark:text-gray-400">
+          <Link href={'#Contact'}>contact</Link>
+        </li>
+      </ul>
+    </motion.nav>
+  );
 };
