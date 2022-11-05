e26889266f599473b73fae78e181b5ae2041db38
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-11-05 15:56:30 +0700

    chores: add twitter OG for better social share

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,6 +28,14 @@ const Home = ({
           content="https://cdn.sanity.io/images/lnyy2568/production/efd4bddda3c74c14824ca7da019521ec3ceefb30-3000x2000.png"
         />
         <meta
+          name="twitter:image"
+          content="https://cdn.sanity.io/images/lnyy2568/production/efd4bddda3c74c14824ca7da019521ec3ceefb30-3000x2000.png"
+        />
+        <meta
+          property="twitter:image:alt"
+          content="Website on display with laptop, mobile, tablet"
+        />
+        <meta
           property="og:image:alt"
           content="Website on display with laptop, mobile, tablet"
         />
@@ -35,6 +43,13 @@ const Home = ({
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://haonguyen.tech" />
         <meta property="og:image:type" content="image/png" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@haonguyen_tech" />
+        <meta name="twitter:title" content="Hao Nguyen Personal Portfolio" />
+        <meta
+          name="twitter:description"
+          content="Hi my name is Hao Nguyen.I'm a front-end developer based in Vietnam Welcome to my portfolio"
+        />
         <meta name="robots" content="index, follow" />
         <title>Hao Portfolio</title>
         <link rel="canonical" href="https://haonguyen.tech" />
