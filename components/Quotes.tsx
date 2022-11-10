0510c1f7ac37a186c9381d58f8d2181779817b67
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-11-10 08:43:01 +0700

    chores: improve contrast text

--- components/Quotes.tsx
+++ components/Quotes.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Quotes = () => {
   return (
     <>
-      <q className="font-semibold">Start with Hope</q>
+      <q className="font-semibold dark:text-white">Start with Hope</q>
       <br />
       <cite>Michael Jordan - The Last Dance </cite>
     </>
